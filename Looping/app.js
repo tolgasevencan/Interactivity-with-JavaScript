8595d945980a2f027e67e6956ca1ebf99dbc80c3
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-04-27 08:14:13 +0200

    working on looping and condition

--- Looping/app.js
+++ Looping/app.js
@@ -1,5 +1,33 @@
+/*
 var arr = [0, 1, 2];
 
 //arr[1] = arr[1] + arr[1];
 arr[1] += arr[1];
 console.log(arr[1]);
+
+
+x = 3
+
+if (x < 2) {
+    console.log('Nope!');
+} else {
+    console.log('Yeap!');
+}
+
+*/
+
+var grades = [2, 5, , , 9, 8, , 8];
+var sum = 0;
+var count = 0;
+
+if (grades.length > 0) {
+    for (let index = 0; index < grades.length; index++) {
+        if (grades[index] != undefined) {
+            sum += grades[index];
+            count = count + 1;
+        }
+    }
+    document.write(sum / count);
+} else {
+    document.write("Empty array");
+}
